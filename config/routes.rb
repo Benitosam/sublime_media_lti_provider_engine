cbc7af3b30079bafd0ff45cb6a1d42767269f185
Benitosam <benito@zionstar.in>
2022-06-13 17:09:49 +0530

    send org_id in launch url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 LtiProvider::Engine.routes.draw do
   get "/cookie_test", to: "lti#cookie_test", as: "cookie_test"
   get "/consume_launch", to: "lti#consume_launch", as: "consume_launch"
-  post "/launch", to: "lti#launch", as: "lti_launch"
+  post "/:organization_id/launch", to: "lti#launch", as: "lti_launch"
   get "/configure(.:format)", to: "lti#configure", as: "lti_configure"
 end
