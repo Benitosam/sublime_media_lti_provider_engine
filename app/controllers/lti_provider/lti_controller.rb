cd4b90f8f57f09859be70f06b970b00ccd2f8db3
Benitosam <benito@zionstar.in>
2022-10-27 16:30:45 +0530

    key set in session

--- app/controllers/lti_provider/lti_controller.rb
+++ app/controllers/lti_provider/lti_controller.rb
@@ -64,6 +64,7 @@ module LtiProvider
       session[:canvas_course_id] = params[:custom_canvas_course_id]
       session[:launch_presentation_return_url] = params[:launch_presentation_return_url]
       session[:ext_roles] = params[:ext_roles]
+      session[:key] = params[:oauth_consumer_key]
       # session[:organization_id] = params[:organization_id]
       # session[:app_id] = params[:app_id]
     end
